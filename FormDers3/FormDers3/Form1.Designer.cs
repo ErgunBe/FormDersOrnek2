511285cd487302d2a11b2bfc94d742245d4fb682
ErgunBe <ergun.besli@hotmail.com>
2024-05-19 01:30:18 +0300

    FormOrnek2

---
+++ FormDers3/FormDers3/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FormDers3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            panel1 = new Panel();
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(383, 280);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Baharatlar";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(35, 58);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(84, 27);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Ketcap";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(34, 100);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(100, 27);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "Mayonez";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(35, 142);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(80, 27);
+            checkBox3.TabIndex = 3;
+            checkBox3.Text = "Hardal";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(77, 30);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(71, 27);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Erkek";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(77, 80);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(73, 27);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Kadın";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 192, 192);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(434, 30);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(222, 136);
+            panel1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(434, 210);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(222, 143);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(797, 493);
+            Controls.Add(richTextBox1);
+            Controls.Add(panel1);
+            Controls.Add(groupBox1);
+            Font = new Font("Corbel", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ FormDers3/FormDers3/Form1.cs
@@ -0,0 +1,15 @@
+namespace FormDers3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
